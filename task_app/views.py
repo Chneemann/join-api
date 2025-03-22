1d7b836eb6304ebb04fb460f3b04f683096d5a21
Chneemann <andre-kempf@mailbox.org>
2025-03-22 06:39:53 +0100

    feat: implement filtering of tasks by status in the TaskViewSet class

--- task_app/views.py
+++ task_app/views.py
@@ -3,8 +3,14 @@ from .models import Task, SubTask, AssignedTask
 from .serializers import TaskSerializer, SubTaskSerializer, AssignedTaskSerializer
 
 class TaskViewSet(viewsets.ModelViewSet):
-    queryset = Task.objects.all()
+    queryset = Task.objects.all()  
     serializer_class = TaskSerializer
+
+    def get_queryset(self):
+        status = self.request.query_params.get('status', None)
+        if status:
+            return Task.objects.filter(status=status)
+        return Task.objects.all()
 
 class SubTaskViewSet(viewsets.ModelViewSet):
     queryset = SubTask.objects.all()
